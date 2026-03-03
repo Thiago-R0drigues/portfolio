7286834e2221343173a0aac9812fae497c09324b
Thiago Oliveira <trodrigues361@gmail.com>
2026-03-03 19:41:19 -0400

    main styles udpated

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,13 @@ function App() {
 
 
   return (
-    <>
-      <div className='h-screen font-mono flex justify-between p-3 px-10 bg-[#09182E] pt-5'>
-        <div>
+      <div className='font-mono flex flex-col justify-between p-3 px-10 bg-[#09182E] pt-5'>
+        <div className='flex flex-row justify-around'>
           <h1 className='text-green-600 text-xl hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer'><a href="https://www.linkedin.com/in/thiagorodrigues361/" target="_blank">Thiago Oliveira</a></h1>
           <Nav/>
         </div>
-        <About/>
+        <div className='flex justify-center'><About/></div>
       </div>
-
-    </>
   )
 }
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='font-mono'>
+    <div className='font-mono flex flex-col gap-8 h-screen pt-50 '>
       <p className='text-3xl font-black text-gray-300'>Hi, I'm</p>
       <p className='text-5xl font-black text-gray-500'>Thiago Oliveira</p>
       <p className='text-2xl text-gray-400  w-250 text-justify font-mono'>Front-end Developer focused on building <span className='underline'>modern and responsive user interfaces.</span> I enjoy turning <span className='font-black'>ideas</span> into <span className='font-black'>real-world applications</span> using <span className='text-blue-400'>React</span> and <span className='text-pink-400'>Tailwind</span>.</p>
       <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer">Projects</button>
     </div>
-
   )
 }
 
