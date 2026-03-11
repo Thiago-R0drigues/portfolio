406411533c92d4132c7aadfb5b7cd7152bd6ea9e
Thiago Oliveira <trodrigues361@gmail.com>
2026-03-11 18:28:34 -0400

    Commenced responsivity to mobile (to be improved - a lot...)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div className='font-mono flex flex-col gap-8 pt-50 '>
-      <p className='text-3xl font-black text-gray-300 hover:cursor-default'>Hi, I'm</p>
-      <p className='text-5xl font-black text-gray-500 hover:cursor-default'><a href="https://www.linkedin.com/in/thiagorodrigues361/" target="_blank">Thiago Oliveira</a></p>
-      <p className='text-2xl text-gray-400  w-250 text-justify font-mono hover:cursor-default'>Front-end Developer focused on building <span className='underline'>modern and responsive user interfaces.</span> I enjoy turning <span className='font-black'>ideas</span> into <span className='font-black'>real-world applications</span> using <span className='text-blue-400 hover:text-blue-500 duration-400 ease-in-out'>React</span> and <span className='text-pink-400 hover:text-pink-500 duration-400 ease-in-out'>Tailwind</span>.</p>
+      <p className='text-3xl font-black text-gray-300 hover:cursor-default max-[480px]:text-center '>Hi, I'm</p>
+      <p className='text-5xl font-black text-gray-500 hover:cursor-default max-[480px]:text-center max-[480px]:text-3xl'><a href="https://www.linkedin.com/in/thiagorodrigues361/" target="_blank">Thiago Oliveira</a></p>
+      <p className='text-2xl text-gray-400  w-250 text-justify font-mono hover:cursor-default max-[480px]:w-90 max-[480px]:text-center max-[480px]:tracking-tighter max-[480px]:p-8'>Front-end Developer focused on building <span className='underline'>modern and responsive user interfaces.</span> I enjoy turning <span className='font-black'>ideas</span> into <span className='font-black'>real-world applications</span> using <span className='text-blue-400 hover:text-blue-500 duration-400 ease-in-out'>React</span> and <span className='text-pink-400 hover:text-pink-500 duration-400 ease-in-out'>Tailwind</span>.</p>
       <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer">Projects</button>
     </div>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,9 @@ function Nav() {
     return (
         <div>
             <ul className="flex flex-row gap-20">
-                <li className="cursor-pointer hover:border-green-600 hover:border-dotted duration-200"><span className="text-green-600">01.</span> <span className="text-gray-300">About</span></li>
-                <li className="cursor-pointer hover:border-green-600 hover:border-dotted duration-200"><span className="text-green-600">02.</span> <span className="text-gray-300">Projects</span></li>
-                <li className="cursor-pointer hover:border-green-600 hover:border-dotted duration-200"><span className="text-green-600">03.</span> <span className="text-gray-300">Contact</span></li>
+                <li className="cursor-pointer"><span className="text-green-600">01.</span> <span className="text-gray-300">About</span></li>
+                <li className="cursor-pointer"><span className="text-green-600">02.</span> <span className="text-gray-300">Projects</span></li>
+                <li className="cursor-pointer"><span className="text-green-600">03.</span> <span className="text-gray-300">Contact</span></li>
                 
             </ul>
         </div>
