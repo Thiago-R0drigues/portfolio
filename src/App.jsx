05b2f789143b1353c355df335035c3ef8ee768bd
Thiago Oliveira <trodrigues361@gmail.com>
2026-03-11 16:47:25 -0400

    Adjusted position from absolute to fixed at App.jsx main div and removed hover effect on Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/Projects'
 function App() {
 
   return (
-      <div className='font-mono flex flex-col bg-[#09182E] absolute h-full w-full'>
+      <div className='font-mono flex flex-col bg-[#09182E] fixed h-full w-full'>
   
         <div className="flex justify-center pt-8"><Nav/></div>  
         <div className='flex justify-center'><About/></div>
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <p>linkedin logo</p>
+      <p>github logo</p>
+      <p>gmail logo</p>
+    </div>
+  )
+}
+
+export default Contact
