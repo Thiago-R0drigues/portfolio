b49400fb7f74cd86da24b1a4c54f5eb198ce2e1c
Thiago Oliveira <trodrigues361@gmail.com>
2026-04-10 12:40:36 -0400

    Update: handled project button at About component. Next updates on contact component

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import { useState } from 'react'
 import './index.css'
 
 //NAVIGATION COMPONENT
-const Nav = ({ About, Projects, Contact}) => {
+const Nav = ({ About, Projects, Contact, activeSection}) => {
 
-  const [active, setActive] = useState('about');
-
+ 
   return (
     <div>
       <ul className="flex flex-row gap-30 text-xl font-mono">
         <li 
-        className={active === 'about' ? 
+        className={activeSection === 'about' ? 
           'cursor-pointer border-b-2 border-b-gray-400' : 'cursor-pointer'} 
         onClick={()=> {
-          setActive('about');
           About();
         }}><span className="text-green-600">01.</span> <span className="text-gray-300">About</span>
         </li>
 
         <li 
-        className={active === 'projects' ? 
+        className={activeSection === 'projects' ? 
           'cursor-pointer border-b-2 border-b-gray-400' : 'cursor-pointer'}
         onClick={()=> {
-          setActive('projects');
           Projects();
           
           }}><span className="text-green-600">02.</span> <span className="text-gray-300">Projects</span>
@@ -30,10 +27,9 @@ const Nav = ({ About, Projects, Contact}) => {
 
 
         <li 
-        className={active === 'contact' ?
+        className={activeSection === 'contact' ?
           'cursor-pointer border-b-2 border-b-gray-400' : 'cursor-pointer'}
         onClick={()=> {
-          setActive('contact');
           Contact();
         }}><span className="text-green-600">03.</span> <span className="text-gray-300">Contact</span>
         </li>
@@ -43,18 +39,17 @@ const Nav = ({ About, Projects, Contact}) => {
 }
 
 //ABOUT COMPONENT
-const About = ({Projects, handleProject}) => {
+const About = ({onClick}) => {
 
   return (
     <div className='font-mono flex flex-col gap-8 pt-50 '>
       <p className='text-3xl font-black text-gray-300 hover:cursor-default max-[480px]:text-center '>Hi, I'm</p>
       <p className='text-5xl font-black text-gray-500 hover:cursor-default max-[480px]:text-center max-[480px]:text-3xl'><a href="https://www.linkedin.com/in/thiagorodrigues361/" target="_blank">Thiago Oliveira</a></p>
       <p className='text-2xl text-gray-400  w-250 text-justify font-mono hover:cursor-default max-[480px]:w-90 max-[480px]:text-center max-[480px]:tracking-tighter max-[480px]:p-8'>Front-end Developer focused on building <span className='underline'>modern</span> and <span className='underline'>responsive</span> <span className="underline">user</span> <span className="underline">interfaces</span>. I enjoy turning <span className='font-black'>ideas</span> into <span className='font-black'>real-world applications</span> using <span className='text-blue-400 hover:text-blue-500 duration-400 ease-in-out'>React</span> and <span className='text-pink-400 hover:text-pink-500 duration-400 ease-in-out'>Tailwind</span>.</p>
-      <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer" onClick={()=>{Projects(); handleProject}}>Projects</button>
+      <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer" onClick={onClick}>Projects</button>
     </div>
   )
 }
-
 
 const Projects = () => {
   return (
@@ -113,14 +108,14 @@ function App() {
   return (
     <div className='font-mono flex flex-col bg-[#09182E] fixed h-full w-full'>
       <div className="flex justify-center pt-8">
-        <Nav About={activateAbout} Projects={activateProjects} Contact={activateContact} />
+        <Nav About={activateAbout} Projects={activateProjects} Contact={activateContact} activeSection={activeSection} />
       </div>
 
       <div className='flex justify-center'>
-        {activeSection === 'about' && <About Projects={activateProjects} />}
-        {activeSection === 'projects' && <Projects />}
+        {activeSection === 'about' && <About onClick={activateProjects}/>}
+        {activeSection === 'projects' && <Projects/>}
         {activeSection === 'contact' && <Contact />}
       </div>
     </div>)}
 
-      export default App
+      export default App;
