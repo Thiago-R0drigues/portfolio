d1d2f4e62e8886bcd9209b5247faaf2e0851dd81
Thiago Oliveira <trodrigues361@gmail.com>
2026-04-10 12:17:28 -0400

    Update: added some images at contact component; To Do: Fix project button at About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import './index.css'
 
-
-
 //NAVIGATION COMPONENT
-const Nav = ({ About, Projects, Contact }) => {
+const Nav = ({ About, Projects, Contact}) => {
 
   const [active, setActive] = useState('about');
 
@@ -26,8 +24,10 @@ const Nav = ({ About, Projects, Contact }) => {
         onClick={()=> {
           setActive('projects');
           Projects();
+          
           }}><span className="text-green-600">02.</span> <span className="text-gray-300">Projects</span>
         </li>
+
 
         <li 
         className={active === 'contact' ?
@@ -43,33 +43,53 @@ const Nav = ({ About, Projects, Contact }) => {
 }
 
 //ABOUT COMPONENT
-const About = () => {
+const About = ({Projects, handleProject}) => {
 
   return (
     <div className='font-mono flex flex-col gap-8 pt-50 '>
       <p className='text-3xl font-black text-gray-300 hover:cursor-default max-[480px]:text-center '>Hi, I'm</p>
       <p className='text-5xl font-black text-gray-500 hover:cursor-default max-[480px]:text-center max-[480px]:text-3xl'><a href="https://www.linkedin.com/in/thiagorodrigues361/" target="_blank">Thiago Oliveira</a></p>
       <p className='text-2xl text-gray-400  w-250 text-justify font-mono hover:cursor-default max-[480px]:w-90 max-[480px]:text-center max-[480px]:tracking-tighter max-[480px]:p-8'>Front-end Developer focused on building <span className='underline'>modern</span> and <span className='underline'>responsive</span> <span className="underline">user</span> <span className="underline">interfaces</span>. I enjoy turning <span className='font-black'>ideas</span> into <span className='font-black'>real-world applications</span> using <span className='text-blue-400 hover:text-blue-500 duration-400 ease-in-out'>React</span> and <span className='text-pink-400 hover:text-pink-500 duration-400 ease-in-out'>Tailwind</span>.</p>
-      <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer">Projects</button>
+      <button className="border-2 border-green-600 text-green-600 w-60 p-5 mt-5 hover:border-green-400 hover:text-green-400 transition duration-50 ease-in-out hover:cursor-pointer" onClick={()=>{Projects(); handleProject}}>Projects</button>
     </div>
   )
 }
+
 
 const Projects = () => {
   return (
     <div>
-
+      <h1 className='text-white'>PROJECTS</h1>
     </div>
   )
 }
 
+const cardList = [
+  {title: 'Card1', src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/960px-GitHub_Invertocat_Logo.svg.png', alt: 'Github image'}, 
+  {title: 'Card2', src: 'https://images.icon-icons.com/3042/PNG/512/linkedin_logo_icon_189254.png', alt: 'Linkedin image'}, 
+  {title: 'Card2', src: 'https://via.placeholder.com/150', alt: 'Gmail image'},
+];
+
+const Card = ({title, src, alt}) => {
+
+    return (
+      <div className='px-10 py-3 flex justify-between'>
+          <img src={src} alt={alt} className='w-30 cursor-pointer'/>
+          {/* <p>{title}</p> */}
+      </div>          
+
+    )
+  }
 //CONTACT COMPONENT
 const Contact = () => {
+
   return (
-    <div>
-      <p><img src="" alt="" /></p>
-      <p>github logo</p>
-      <p>gmail logo</p>
+    <div className='text-white border-none'>
+      <ul>
+        <li><Card src={cardList[0].src} alt={cardList[0].alt}/></li>
+        <li><Card src={cardList[1].src} alt={cardList[1].alt}/></li>
+        <li><Card src={cardList[2].src} alt={cardList[2].alt}/></li>
+      </ul>
     </div>
   )
 }
@@ -97,7 +117,7 @@ function App() {
       </div>
 
       <div className='flex justify-center'>
-        {activeSection === 'about' && <About />}
+        {activeSection === 'about' && <About Projects={activateProjects} />}
         {activeSection === 'projects' && <Projects />}
         {activeSection === 'contact' && <Contact />}
       </div>
